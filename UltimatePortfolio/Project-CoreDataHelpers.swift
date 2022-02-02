6e26115fc530282d2baebf27671921eb25842f6d
Yamasaki Kouya <koya2170@gmail.com>
2022-02-03 00:22:02 +0900

    5. Cleaning up Core Data

---
+++ UltimatePortfolio/Project-CoreDataHelpers.swift
@@ -0,0 +1,67 @@
+//
+//  Project-CoreDataHelpers.swift
+//  UltimatePortfolio
+//
+//  Created by 山崎宏哉 on 2022/02/02.
+//
+
+import Foundation
+
+extension Project {
+    var projectTitle: String {
+        title ?? "New Project"
+    }
+
+    var projectDetail: String {
+        detail ?? ""
+    }
+
+    var projectColor: String {
+        color ?? "Light Blue"
+    }
+
+    var projectItems: [Item] {
+        let itemsArray = items?.allObjects as? [Item] ?? []
+
+        return itemsArray.sorted { first, second in
+            if !first.completed {
+                if second.completed {
+                    return true
+                }
+            } else if first.completed {
+                if !second.completed {
+                    return false
+                }
+            }
+
+            if first.priority > second.priority {
+                return true
+            } else if first.priority < second.priority {
+                return false
+            }
+
+            return first.itemCreationDate < second.itemCreationDate
+        }
+    }
+
+    var completionAmount: Double {
+        let originalItems = items?.allObjects as? [Item] ?? []
+        guard !originalItems.isEmpty else { return 0 }
+
+        let completedItems = originalItems.filter(\.completed) // $0.completed == true
+        return Double(completedItems.count) / Double(originalItems.count)
+    }
+
+    static var example: Project {
+        let controller = DataController(inMemory: true)
+        let viewContext = controller.container.viewContext
+
+        let project = Project(context: viewContext)
+        project.title = "Example Project"
+        project.detail = "This is an example project"
+        project.closed = true
+        project.creationDate = Date()
+
+        return project
+    }
+}
