06fde9e68b969ced8e6882c7f40ac83395e03152
Yamasaki Kouya <koya2170@gmail.com>
2022-02-18 23:02:07 +0900

    17. Creating the Home view

---
+++ UltimatePortfolio/Color-Addition.swift
@@ -0,0 +1,14 @@
+//
+//  Color-Addition.swift
+//  UltimatePortfolio
+//
+//  Created by 山崎宏哉 on 2022/02/18.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemBackground)
+}
