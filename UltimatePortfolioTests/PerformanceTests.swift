91b3f48f94fe6a2c99fe90f573185239cae56ef8
Yamasaki Kouya <koya2170@gmail.com>
2022-03-05 23:47:43 +0900

    31. Measuring performance

---
+++ UltimatePortfolioTests/PerformanceTests.swift
@@ -0,0 +1,29 @@
+//
+//  PerformanceTests.swift
+//  UltimatePortfolioTests
+//
+//  Created by 山崎宏哉 on 2022/03/05.
+//
+
+import XCTest
+@testable import UltimatePortfolio
+
+class PerformanceTests: BaseTestCase {
+    func testAwardCalculationPerformance() throws {
+        // Create a significant amount of test data
+        for _ in 1...100 {
+            try dataController.createSampleData()
+        }
+
+        // Simulate lots of awards to check
+        let awards = Array(repeating: Award.allAwards, count: 25).joined()
+        XCTAssertEqual(
+            awards.count, 500,
+            "This checks the number of awards is constant. Change this if you add new awards."
+        )
+
+        measure {
+            _ = awards.filter(dataController.hasEarned)
+        }
+    }
+}
